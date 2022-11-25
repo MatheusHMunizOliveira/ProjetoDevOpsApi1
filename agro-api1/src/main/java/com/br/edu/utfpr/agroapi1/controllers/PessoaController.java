d46d46ed92a25c2b24314e309480e40b0e1424bb
Matheus Oliveira <matheusoliveira.2018@alunos.utfpr.edu.br>
2022-11-25 11:24:10 -0300

    criando metodo de validar pessoa e senha

--- agro-api1/src/main/java/com/br/edu/utfpr/agroapi1/controllers/PessoaController.java
+++ agro-api1/src/main/java/com/br/edu/utfpr/agroapi1/controllers/PessoaController.java
@@ -1,10 +1,12 @@
 package com.br.edu.utfpr.agroapi1.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.br.edu.utfpr.agroapi1.model.Pessoa;
 import com.br.edu.utfpr.agroapi1.repositories.PessoaRepository;
 
+import org.springframework.http.HttpStatus;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -57,6 +60,24 @@ public class PessoaController {
     @DeleteMapping
     public void excluir(@RequestBody Pessoa pessoa) {
         repository.delete(pessoa);
+    }
+
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String email, @RequestParam String senha){
+
+       
+        boolean valid = false;
+        // Verfica se o usuario existe
+        Optional<Pessoa> opPessoa = repository.findByEmail(email);
+        if (opPessoa.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(valid);
+        }
+        //Verifica se a senha informada condiz com o usuario
+        valid = encoder.matches(senha, opPessoa.get().getSenha());
+        //se a senha for valida vai atribuir status OK se nao NaoAutorizado
+        HttpStatus status = valid ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(status).body(valid);
     }
 
     // fim metodos
